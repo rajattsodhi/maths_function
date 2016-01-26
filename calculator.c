7de4befca84a54defcd538c928e092af206df4b1
Rajatt Sodhi <P14166609@GWHXX583LIN010W.lec-admin.dmu.ac.uk>
2016-01-26 11:39:46 +0000

    added calculator.c with new remainder function

---
+++ calculator.c
@@ -0,0 +1,58 @@
+int main()
+{
+    char o;
+    float num1,num2;
+    int i;
+    float arr[5],num;
+    printf("Enter operator either (+) or (-) or (*) or divide (:) or average (a) or remainder (%) ");
+    scanf("%c",&o);
+    
+    switch(o) {
+        case '+':
+	    printf("Enter two operands: ");
+    	    scanf("%f%f",&num1,&num2);
+            printf("%.1f + %.1f = %.1f",num1, num2, num1+num2);
+            break;
+	case '-':
+	    printf("Enter two operands: ");
+            scanf("%f%f",&num1,&num2);
+            printf("%.1f - %.1f = %.1f",num1, num2, num1-num2);
+            break;
+	 case '*':
+	    printf("Enter two operands: ");
+            scanf("%f%f",&num1,&num2);
+            printf("%.1f * %.1f = %.1f",num1, num2, num1*num2);
+            break;
+	case '/':
+	    printf("Enter two operands: ");
+            scanf("%f%f",&num1,&num2);
+            printf("%.1f / %.1f = %.1f",num1, num2, num1/num2);
+            break;
+        case 'a':
+	    i=0;
+	    num=0;
+
+	   
+	    while(num!=-1 || i<5)
+	    printf("Enter a number, -1 to stop maximum 5 numbers and minimum 1 number: ");
+            scanf("%f",&num);
+	    arr[i]=num;
+            printf("%.1f / %.1f = %.1f",num1, num2, num1/num2);
+            break;
+        case '%':
+	    printf("Enter two operands: ");
+            scanf("%f%f",&num1,&num2);
+            printf("%.1f % %.1f = %.1f",num1, num2, num1%num2);
+            break;
+        
+       
+        
+        
+        
+        default:
+            /* If operator is other than +, -, * or /, error message is shown */
+            printf("Error! operator is not correct");
+            break;
+    }
+    return 0;
+}
