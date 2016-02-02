86acc7094a31a6777094f1c7b7d92a3a7bfbeac1
Rajatt Sodhi <P14166609@GWHXX583LIN009W.lec-admin.dmu.ac.uk>
2016-02-02 11:28:45 +0000

    added max and min function to calculator.c

--- calculator.c
+++ calculator.c
@@ -2,9 +2,11 @@ int main()
 {
     char o;
     float num1,num2;
+    float nums[5];
+    float max,min;
     int i;
     float arr[5],num;
-    printf("Enter operator either (+) or (-) or (*) or divide (:) or average (a) or remainder (%) ");
+    printf("Enter operator either (+) or (-) or (*) or divide (:) or average (a) or remainder (%) or (m)in and max ");
     scanf("%c",&o);
     
     switch(o) {
@@ -43,6 +45,22 @@ int main()
 	    printf("Enter two operands: ");
             scanf("%f%f",&num1,&num2);
             printf("%.1f % %.1f = %.1f",num1, num2, num1%num2);
+            break;
+       case 'm':
+	    printf("Enter 5 operands: ");
+            for(i=0;i<5;i++)
+            scanf("%f",&nums[0]);
+	    float max, min;
+	    max=nums[0];
+	    min=nums[0];
+	    for(i=1;i<5;i++){
+		if (max<nums[i])
+			max=nums[i];
+		if (min>nums[i])
+			min=nums[i];
+	    }
+	    
+            printf("min = %.1f, max = %.1f",min,max);
             break;
         
        
